5add3436b601d4dd54d07b3a8c22b7a9ebbd7bdb
Kacper <kacper.blak@outlook.com>
2018-10-01 19:18:20 +0200

    Add validation for registration

---
+++ validation/is-empty.js
@@ -0,0 +1,8 @@
+// Checks undefined, null, empty objects & strings
+const isEmpty = value =>
+  value === undefined ||
+  value === null ||
+  (typeof value === "object" && Object.keys(value).length === 0) ||
+  (typeof value === "string" && value.trim().length === 0);
+
+module.exports = isEmpty;
